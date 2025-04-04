1c494935228ee7715f0ad258504085f5e9c61828
saurabh yadav <saurabh@antheminfotech.com>
2025-04-04 10:19:35 +0530

    title, description chnage #04042025

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'FabCaptial',
+  description: 'FabCapital is a fintech company that provides a range of financial services to businesses and consumers.',
+  generator: 'FabCapital',
 }
 
 export default function RootLayout({
